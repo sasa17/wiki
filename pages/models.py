8a2e51025c0ef3e82cc99d35d99df523192736d4
Sarah <s.f.alashban@gmail.com>
2020-01-14 18:38:44 +0300

    Wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	last_update = models.DateTimeField(auto_now_add=True)
+
+	def get_absolute_url(self):
+		return "/pages/%i/" %self.id
